e22d33f2741f0b946c8dc81392d996f010681ff8
Kick_Sim <98477056+kick-sim@users.noreply.github.com>
2023-12-31 07:47:54 +0800

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -4,47 +4,50 @@
 #include<queue>
 #include<algorithm>
 using namespace std;
-int n,m,k,a,b;
-vector<int> adj[10001];
-bool visited[1001] = {false,};
-bool visited_b[1001] = {false,};
-void dfs(int v){
-    visited[v] = true;
-    cout << v << " ";
-    for(int next : adj[v]){
-        if(!visited[next]){
+
+int n,m,v,a,b;
+vector<int> adj[1001];
+bool visited_dfs[1001] = {false};
+bool visited_bfs[1001] = {false};
+
+void dfs(int h){
+    visited_dfs[h] = true;
+    cout << h << " ";
+    for(int next : adj[h]){
+        if(!visited_dfs[next]){
             dfs(next);
         }
     }
 }
-void bfs(int v){
+void bfs(int h){
+    visited_bfs[h] = true;
     queue<int> q;
-    visited_b[v] = true;
-    q.push(v);
+    q.push(h);
     while(!q.empty()){
         int front = q.front();
         q.pop();
+        visited_bfs[front] = true;
         cout << front << " ";
         for(int next:adj[front]){
-            if(!visited_b[next]){
-                visited_b[next] = true;
+            if(!visited_bfs[next]){
                 q.push(next);
+                visited_bfs[next] = true;
             }
         }
     }
 }
 
 int main(){
-    cin >> n >> m >> k;
+    cin >> n >> m >> v;
     for(int i = 0; i < m; i++){
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    for(int i = 1; i <= n; i++){
+    for(int i = 1;i <= n; i++){
         sort(adj[i].begin(), adj[i].end());
     }
-    dfs(k);
+    dfs(v);
     cout << "\n";
-    bfs(k);
+    bfs(v);
 }
